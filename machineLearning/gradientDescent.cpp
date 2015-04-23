8f8288321b54e6a131151f750c6c7fff5d6a08b3
Apple <apple@Apples-iMac-2.local>
2015-04-23 18:06:11 +0800

    gradient descent, loss increase

--- machineLearning/gradientDescent.cpp
+++ machineLearning/gradientDescent.cpp
@@ -5,21 +5,21 @@
 	________________________________
   price per share	|   income   |  profit
 	________________________________
-	 0.09    		|  5811.89   |  723.73		(2010-06-30)
+	 0.09 		|  5811.89   |  723.73		(2010-06-30)
 	________________________________
-   	 0.33     		|  14421.93  |  2766.96		(2010-12-31)
+   	 0.33    	|  14421.93  |  2766.96		(2010-12-31)
 	________________________________
-	 0.047   		|  6565.74   |  591.51		(2011-06-30)
+	 0.047 		|  6565.74   |  591.51		(2011-06-30)
 	_________________________________
 	 0.22	    	| 15808.84   |  2748.51		(2011-12-31)
 	_________________________________
-	 0.034		    |  6816.92   |  427.39		(2012-06-30)
+	 0.034		|  6816.92   |  427.39		(2012-06-30)
 	_________________________________
 	 0.096	    	|  15446.51  |  1605.50		(2012-12-31)
 	_________________________________
-	 0.04		    |  6002.91   |  496.79		(2013-06-30)
+	 0.04		|  6002.91   |  496.79		(2013-06-30)
 	_________________________________
-	 0.53	 	    |  46342.22  |  16708.62	(2013-12-31)
+	 0.53	 	|  46342.22  |  16708.62	(2013-12-31)
 	_________________________________
  Output : Predict profit in 2014
 		The ground-truth value is
@@ -49,57 +49,70 @@ int main()
 	
 	//hypothesis result     profit = h(x) = theta_0 + theta_1*x_1 + theta_2*x_2
 	//inititial state theta_0 = theta_1 = theta_2 = 0
-	float theta_0 = 0;
-	float theta_1 = 0;
-	float theta_2 = 0;
+	float theta[3] = {0,0,0};
 	float E = 0;
 	float alpha = 0.01;
 
-	float theta_0_new;
-	float theta_1_new;
-	float theta_2_new;
+	float theta_new[3] = {0,0,0};
 
 	//record the step length
 	float delta[3] = {0,0,0};
 	float eps = 0.1;
 	int count = 1;
-	bool flag;
+	bool flag = true;
 	/*
 	   train theta
 	*/
 	do
 	{
-			for(int i = 0; i < 8; i++)
-			{	
-				E = theta_0 + theta_1 * trainData[i][0] + theta_2 * trainData[i][1] - trainData[i][2];
-			
+			//update parameters theta[0,1,2]
+			for(int j = 0; j < 3; j++)
+			{
+				for(int i = 0; i < 8; i++)
+				{	
+					//sum of differences
+					if(j == 0)
+						E +=  theta[0] + theta[1] * trainData[i][0] + theta[2] * trainData[i][1] - trainData[i][2];
+					else
+					{
+						E += (theta[0] + theta[1] * trainData[i][0] + theta[2] * trainData[i][1] - trainData[i][2]) * trainData[i][j-1];
+					}
+				}
+				cout<<"E is "<<E<<endl;
+				getchar();
+				theta_new[j] = theta[j] - alpha * E;
 
-				theta_0_new = theta_0 - alpha * E * trainData[i][0];
-				theta_1_new = theta_1 - alpha * E * trainData[i][1];
-				theta_2_new = theta_2 - alpha * E * trainData[i][2];
-					
+				//test the step length
+				delta[j] = fabs(theta_new[j] - theta[j]);
+				
+				E = 0;
+			}
+
+			//determine whether stop the program
+			for(int m = 0; m < 3; m++)
+			{
+				if(delta[m] < eps)
+				{
+					flag = false;
+				}
+				else
+					flag = true;
+			}
+			
+			if(flag)
+			{
+				theta[0] = theta_new[0];
+				theta[1] = theta_new[1];
+				theta[2] = theta_new[2];
 			}
 			cout<<"The "<<count<<" time"<<endl;
-			cout<<theta_0_new<<endl;
-			cout<<theta_1_new<<endl;
-			cout<<theta_2_new<<endl;
+			cout<<theta_new[0]<<endl;
+			cout<<theta_new[1]<<endl;
+			cout<<theta_new[2]<<endl;
 			cout<<" "<<endl;
-
-			//test the step length
-			delta[0] = fabs(theta_0_new - theta_0);
-			delta[1] = fabs(theta_1_new - theta_1);
-			delta[2] = fabs(theta_2_new - theta_2);
-
-			if(delta[0] > eps || delta[1] > eps || delta[2] > eps)
-			{
-				flag = true;
-				theta_0 = theta_0_new;
-				theta_1 = theta_1_new;
-				theta_2 = theta_2_new;
-				count++;
-			}
-			else
-				flag = false;
+			
+			getchar();	
+			count++;
 	}while(flag);
 
 	return 0;
