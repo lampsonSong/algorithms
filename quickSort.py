a80d13ba5de6557871fa698b381f67014a1cc6f6
lampsonSong <yuhao915@gmail.com>
2016-10-01 18:59:39 +0800

    small space complexity

--- quickSort.py
+++ quickSort.py
@@ -11,38 +11,46 @@
 
 import pdb
 
-def sort(data):
-	num = len(data)-1
-	print num
-	if(num>1):
-		select_value = data[num/2]
-		print select_value
-		forword_i = 0
-		end_i = num
+def sort(data,forword_i,end_i):
+	
+	if(forword_i >= end_i):
+		return data
 
-		while(forword_i < end_i):
-			while(forword_i < end_i and data[forword_i] < select_value):
-				forword_i += 1
+	select_value = data[forword_i]
+	# print select_value
 
-			while(forword_i < end_i and data[end_i] > select_value):
-				end_i -= 1
+	start = forword_i
+	end = end_i	
 
-			if(forword_i < end_i):
-				data[forword_i],data[end_i] = data[end_i],data[forword_i]
-			
-		# print len(data[:end_i])
-		# print len(data[end_i:])
-		sort(data[:end_i])
-		sort(data[end_i:])
-	else:
-		print data
+	while(forword_i < end_i):
+		
+		while(forword_i < end_i and data[end_i] >= select_value):
+			end_i -= 1
+
+		data[forword_i] = data[end_i]
+
+		while(forword_i < end_i and data[forword_i] <= select_value):
+			forword_i += 1
+
+		data[end_i] = data[forword_i]
+
+	data[forword_i] = select_value
+		
+	# print forword_i,end_i,start,end
+	# pdb.set_trace()
+	sort(data,start,forword_i-1)
+	sort(data,end_i+1,end)
+
+	return data
 
 
 
 
 def main():
 	data = [7,4,5,2,45,6,2,1,3]
-	sort(data)
+	num = len(data)-1
+
+	print sort(data,0,num)
 
 
 if __name__ == "__main__":
